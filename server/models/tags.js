fc9ee6063439fa30c87d32a01c20669304068598
devR1shabh <r1shabh264237215@gmail.com>
2026-02-23 17:41:32 +0530

    Impolemeneted the tag schema

---
+++ server/models/tags.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+
+const tagsSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+    },
+} , {timestamps : true});
+
+module.exports = mongoose.model("Tag" , tagsSchema);
