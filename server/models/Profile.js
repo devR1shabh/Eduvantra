2dc5268ec73dfe21b3db7b147295966d63b69aca
devR1shabh <r1shabh264237215@gmail.com>
2026-02-23 16:13:33 +0530

    Implemented Profile Schema

---
+++ server/models/Profile.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+const profileSchema = new mongoose.Schema({
+    gender:{
+        type:String,
+        enum:["Male" , "Female" , "Other" , "Rather Not Say"],
+    },
+    dateOfBirth:{
+        type:Date,
+    },
+    about:{
+        type:String,
+        trim:true,
+    },
+    contactNumber:{
+        type:String,
+        trim:true,
+        minlength:8,
+        maxlength:15,
+    },
+}, {timestamps : true})
+
+module.exports = mongoose.model("Profile" , profileSchema);
