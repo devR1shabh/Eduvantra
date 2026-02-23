9a633891bc6f31a9d7c357710b98c176b497a611
devR1shabh <r1shabh264237215@gmail.com>
2026-02-23 16:34:22 +0530

    Implemented SubSection Schema

---
+++ server/models/SubSection.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+const subSectionSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    timeDuration:{
+        type:Number,  //in seconds
+        required:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+    },
+    videoUrl:{
+        type:String,
+        required:true,
+    },
+} , {timestamps:true});
+
+module.exports = mongoose.model("SubSection" , subSectionSchema);
