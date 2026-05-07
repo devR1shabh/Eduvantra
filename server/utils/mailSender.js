94776849afc9d98afcac6bb8c2b73ce2e9050773
devR1shabh <r1shabh264237215@gmail.com>
2026-05-07 19:46:00 +0530

    updated nodemailer again a little fix

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -6,7 +6,7 @@ const mailSender = async (email, title, body) => {
 
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
+      port: Number(process.env.MAIL_PORT),
       secure: false,
       auth: {
         user: process.env.MAIL_USER,
